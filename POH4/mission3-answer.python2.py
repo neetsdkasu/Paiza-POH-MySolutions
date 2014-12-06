fbdd2ae58f198f21726903260e049ff341b2eb9c
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2014-12-07 04:56:46 +0900

    Update mission3-answer.python2.py

--- POH4/mission3-answer.python2.py
+++ POH4/mission3-answer.python2.py
@@ -1,15 +1,16 @@
 # coding: utf-8
 (t, n) = [int(i) for i in raw_input().strip().split()]
-maximum = 0
 sum = 0
 a = [0] * n
-for i in xrange(n):
+for i in xrange(t):
     m = int(raw_input())
     a[i] = m
-    if i >= t:
-       sum += m - a[i - t]
-    else:
-        sum += m
+    sum += m
+maximum = sum
+for i in xrange(t, n):
+    m = int(raw_input())
+    a[i] = m
+    sum += m - a[i - t]
     if sum > maximum:
         maximum = sum
 print maximum
