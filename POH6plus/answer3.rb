34cf7de8d573714d5124de666e8eea50e58ba444
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-03 01:25:56 +0900

    Update answer3.rb
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/dd049fde

--- POH6plus/answer3.rb
+++ POH6plus/answer3.rb
@@ -1,8 +1,7 @@
 f,c,s=Hash.new(0),[],""
 w=readlines[1..-1].map{|x|x.chop}
 w.each{|x|f[x]+=1}
-w.sort.each{|x|
-next if f[x]<1
+w.sort.each{|x|next if f[x]<1
 y=x.reverse
 if x==y and f[x]==1
 c.push x
@@ -11,6 +10,5 @@ elsif f.key? y and f[y]>0
 s+=x
 f[x]-=1
 f[y]-=1
-end
-}
+end}
 puts s+(c.size>0?c.sort[0]:"")+s.reverse
