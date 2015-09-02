4819e1a9e517f99af8369d97e314687e73d47adf
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-03 05:56:28 +0900

    バグがあったので直した。ついでに変数宣言まとめて文字数削減（笑）
    Array.IndexOf()のstartIndex指定で本来ならArgumentOutOfRangeExceptionを起こすはずのバグを直した（何故か例外が発生しなかったが（笑））
    ついでに変数宣言まとめて字数減らした
    Result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/609bd79a

--- POH6plus/answer.vb
+++ POH6plus/answer.vb
@@ -1,10 +1,8 @@
 Public Class compiler
   Shared Function StrReverse(s As String) As String
-    Dim a() As Char = s.ToCharArray
-    Dim i As Integer = 0
-    Dim j As Integer = a.Length - 1
+    Dim c As Char, a() As Char = s.ToCharArray, i As Integer = 0, j As Integer = a.Length - 1
     Do While i < j
-        Dim c As Char = a(i)
+        c = a(i)
         a(i) = a(j)
         a(j) = c
         i += 1
@@ -13,21 +11,18 @@ Public Class compiler
     Return New String(a)
   End Function
   Shared Function Main as Integer
-    Dim n As Integer = Int32.Parse(Console.ReadLine)
-    Dim w(n) As String
-    Dim res As String = ""
+    Dim n As Integer = Int32.Parse(Console.ReadLine), w(n + 1) As String, res As String = "", c As String = "", x As String, y As String, k As Integer
     For i As Integer = 1 To n
         w(i) = Console.ReadLine
     Next i
     Array.Sort(w, 1, n)
-    Dim c As String = ""
     For i As Integer = 1 To n
-        Dim x As String = w(i)
+        x = w(i)
         If x.Length = 0 Then
             Continue For
         End If
-        Dim y As String = StrReverse(x)
-        Dim k As Integer = Array.IndexOf(w, y, i + 1)
+        y = StrReverse(x)
+        k = Array.IndexOf(w, y, i + 1)
         If k = -1 Then
             If String.Compare(x, y) = 0 Then
                 If c.Length = 0 Or String.Compare(x, c) < 0 Then
