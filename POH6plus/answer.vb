70824eb32dae468f4ce55c74031f186d46425e27
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-04 05:49:52 +0900

    めいっぱい削ってみた
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/50e70a0f

--- POH6plus/answer.vb
+++ POH6plus/answer.vb
@@ -1,40 +1,6 @@
-Public Class compiler
-  Shared Function StrReverse(s As String) As String
-    Dim c As Char, a() As Char = s.ToCharArray, i As Integer = 0, j As Integer = a.Length - 1
-    Do While i < j
-        c = a(i)
-        a(i) = a(j)
-        a(j) = c
-        i += 1
-        j -= 1
-    Loop
-    Return New String(a)
-  End Function
-  Shared Function Main as Integer
-    Dim n As Integer = Int32.Parse(Console.ReadLine), w(n + 1) As String, res As String = "", c As String = "", x As String, y As String, k As Integer
-    For i As Integer = 1 To n
-        w(i) = Console.ReadLine
-    Next i
-    Array.Sort(w, 1, n)
-    For i As Integer = 1 To n
-        x = w(i)
-        If x.Length = 0 Then
-            Continue For
-        End If
-        y = StrReverse(x)
-        k = Array.IndexOf(w, y, i + 1)
-        If k = -1 Then
-            If String.Compare(x, y) = 0 Then
-                If c.Length = 0 Or String.Compare(x, c) < 0 Then
-                    c = x
-                End If
-            End If
-        Else
-            res += x
-            w(k) = ""
-        End If
-    Next i
-    Console.WriteLine(res + c + StrReverse(res))
-    Return 0
-  End Function
-End Class
+Public Class q:Shared Function u$(s$):Dim c As Char,a()As Char=s.ToCharArray,i%=0,j%=a.Length-1:Do While i<j:c=a(i):a(i)=a(j):a(j)=c:i+=1:j-=1:Loop:Return New String(a):End Function:Shared Function Main%:Dim n%=Int32.Parse(Console.ReadLine),w(n+1) As String,v$="",c$="",x$,y$,k%,i%:For i=1To n:w(i)=Console.ReadLine:Next:Array.Sort(w,1,n):For i=1To n:x=w(i)
+If x.Length=0Then Continue For
+y=u(x):k=Array.IndexOf(w,y,i+1)
+If k=-1Then
+If String.Compare(x, y)=0And(c.Length=0Or String.Compare(x,c)<0)Then c=x
+Else:v+=x:w(k)="":End If:Next:Console.WriteLine(v+c+u(v)):Return 0:End Function:End Class
