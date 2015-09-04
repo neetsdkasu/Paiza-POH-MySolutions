0a6f77b7fcacf245c358bfb90a1b820941556292
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-05 01:48:05 +0900

    1byte削り
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/0e6c91b2

--- POH6plus/answer5.rb
+++ POH6plus/answer5.rb
@@ -3,10 +3,8 @@ s=c=""
 gets.to_i.times{f[gets.chop]+=1}
 f.keys.sort.each{|x|y=x.reverse
 m=[f[x],f[y]].min
-f[x]=0
-if x==y
-c,m=x,m-1if m.odd?&&(c==""||c<x)
-m/=2
-end
+f[x],b=0,x==y
+c,m=x,m-1if b&&m.odd?&&(c==""||c<x)
+b&&m/=2
 m.times{s+=x}}
 $><<s+c+s.reverse
