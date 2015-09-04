8455d8437ecdafe47cd3299ad010743ce8e5afdf
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-05 01:41:03 +0900

    意味ないjoinを止めたらあっさりｗ
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/750122a4

--- POH6plus/answer5.rb
+++ POH6plus/answer5.rb
@@ -1,4 +1,4 @@
-f,s,c=Hash.new(0),[],""
+f,s,c=Hash.new(0),"",""
 gets.to_i.times{f[gets.chop]+=1}
 f.keys.sort.each{|x|y=x.reverse
 m=[f[x],f[y]].min
@@ -7,6 +7,5 @@ if x==y
 c,m=x,m-1if m.odd?&&(c==""||c<x)
 m/=2
 end
-m.times{s+=[x]}}
-t=s.join
-$><<t+c+t.reverse
+m.times{s+=x}}
+$><<s+c+s.reverse
