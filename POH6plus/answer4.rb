e86ae11dc019ca699fc5a550fb372ad0e001f70e
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-05 02:40:01 +0900

    こっちも改行削ってみたけど遠く及ばずｗ
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/7e9144f5

--- POH6plus/answer4.rb
+++ POH6plus/answer4.rb
@@ -1,13 +1,1 @@
-c,s,w=[],"",readlines[1..-1].map{|x|x.chop}.sort.reverse
-while w.size>0
-x=w.pop
-y=x.reverse
-n=w.find_index y
-if n.nil?
-c+=[x]if x==y
-else
-s+=x
-w.delete_at n
-end
-end
-$><<s+(c.size>0?c.sort[0]:"")+s.reverse
+c,s,w=[],"",readlines[1..-1].map{|x|x.chop}.sort.reverse;while w.size>0;x=w.pop;y=x.reverse;n=w.find_index y;if n.nil?;c+=[x]if x==y;else;s+=x;w.delete_at n;end;end;$><<s+(c.size>0?c.sort[0]:"")+s.reverse
