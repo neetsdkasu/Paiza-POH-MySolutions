ace5b3a9fb191def6541dd54dd6dd237bf7457eb
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-05 01:58:42 +0900

    ついに200byte切ったどー！
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/b691f877

--- POH6plus/answer5.rb
+++ POH6plus/answer5.rb
@@ -2,7 +2,7 @@ f,s=Hash.new(0),c=""
 gets.to_i.times{f[gets.chop]+=1}
 f.keys.sort.each{|x|y=x.reverse
 m,f[x],b=[f[x],f[y]].min,0,x==y
-c,m=x,m-1if b&&m.odd?&&(c==""||c<x)
+c,m=x,m-1if b&&m&1&&(c==""||c<x)
 b&&m/=2
 m.times{s+=x}}
 $><<s+c+s.reverse
