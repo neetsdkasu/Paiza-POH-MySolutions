462e9d02509b059d23833196be7572373b65c965
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-05 05:23:56 +0900

    answer5.rbのバグ修正版、あえて別ファイル
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/8cf65053

---
+++ POH6plus/answer6.rb
@@ -0,0 +1,8 @@
+f,s=Hash.new(0),c=""
+gets.to_i.times{f[gets.chop]+=1}
+f.keys.sort.map{|x|y=x.reverse
+m,f[x],b=[f[x],f[y]].min,0,x==y
+c,m=x,m-1if b&&m.odd?&&(c==""||x<c)
+b&&m/=2
+m.times{s+=x}}
+$><<s+c+s.reverse
