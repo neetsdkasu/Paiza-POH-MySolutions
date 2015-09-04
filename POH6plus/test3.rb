511cdba80075526d8be2c381c799fbcea0bc7113
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-05 05:46:06 +0900

    改行削除
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/6c9149e8

--- POH6plus/test3.rb
+++ POH6plus/test3.rb
@@ -1,7 +1,1 @@
-f,s=Hash.new(0),c=""
-gets.to_i.times{f[gets.chop]+=1}
-f.keys.sort.map{|x|y=x.reverse
-m,f[x]=[f[x],f[y]].min,0
-c,m=x,m/2 if x==y
-m.times{s+=x}}
-$><<s+c+s.reverse
+f,s=Hash.new(0),c="";gets.to_i.times{f[gets.chop]+=1};f.keys.sort.map{|x|y=x.reverse;m,f[x]=[f[x],f[y]].min,0;c,m=x,m/2if x==y;m.times{s+=x}};$><<s+c+s.reverse
