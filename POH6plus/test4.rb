32022da3a56b16185d15012bf450e010955d9b59
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-05 06:31:23 +0900

    逆順が存在する単語は常に対の相手と同数という仮説の元に…
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/5b33441b

---
+++ POH6plus/test4.rb
@@ -0,0 +1,9 @@
+f,s=Hash.new(0),c=""
+gets.to_i.times{f[gets.chop]+=1}
+f.keys.sort.map{|x|y=x.reverse
+next if f[x]!=f[y]
+m,f[x],b=[f[x],f[y]].min,0,x==y
+c,m=x,m-1if b&&m.odd?&&(c==""||x<c)
+b&&m/=2
+m.times{s+=x}}
+$><<s+c+s.reverse
