0844e1d22af03fde37c99d0ed6f8093d7d91611b
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-05 19:02:44 +0900

    判定順序変えたら、さらに縮まった
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/0cfc3440

--- POH6plus/answer6.rb
+++ POH6plus/answer6.rb
@@ -1,1 +1,1 @@
-f=Hash.new 0;s=c="";gets.to_i.times{f[gets.chop]+=1};f.sort.map{|x,m|y=x.reverse;m,f[x],b=[m,f[y]].min,0,x==y;c,m=x,m-1if b&&m.odd?&&(c==""||x<c);b&&m/=2;s+=x.*m};$><<s+c+s.reverse
+f=Hash.new 0;s=c="";gets.to_i.times{f[gets.chop]+=1};f.sort.map{|x,m|y=x.reverse;m,f[x],b=[m,f[y]].min,0,x==y;c,m=x,m-1if(c==""||x<c)&b&m.odd?;b&&m/=2;s+=x.*m};$><<s+c+s.reverse
