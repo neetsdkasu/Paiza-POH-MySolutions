d7aa025abc45aae2193fa14994f17aac7dfed80f
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-05 18:37:01 +0900

    変数初期化で縮められた件
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/0c3cb8f7

--- POH6plus/test5.rb
+++ POH6plus/test5.rb
@@ -1,1 +1,9 @@
-f,s=Hash.new(0),c="";gets.to_i.times{f[[x=gets.chop,y=x.reverse].min]+=1;x==y&&c=x};f.sort.map{|k,v|s+=k.*v/2};$><<s+c+s.reverse
+f=Hash.new 0;s=c=""
+gets.to_i.times{
+    f[[x=gets.chop,y=x.reverse].min]+=1
+    x==y&&c=x
+}
+f.sort.map{|k,v|
+s+=k.*v/2
+}
+$><<s+c+s.reverse
