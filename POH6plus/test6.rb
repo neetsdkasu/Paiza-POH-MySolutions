04eb86bffe82a7e36941f2ac57e0a7437607b12f
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-06 05:33:17 +0900

    Update test6.rb
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/7aa551b5

--- POH6plus/test6.rb
+++ POH6plus/test6.rb
@@ -1,18 +1,16 @@
 f = Hash.new 0
 g = {}
 c = ""
-a = Array.new(1001, "")
-i = 0
-j = 1001
+a = []
+b = []
 ARGF.read.split[1..-1].map{|x|
     f[[x,y=x.reverse].min] += 1;
     g[x] = y
     x==y&&c=y
 }
 f.sort.map{|k,v|
-a.fill(k, i, v>>1)
-a.fill(g[k], j -= v>>1, v>>1)
-i+=v>>1
+a += [k].*v>>1
+b = ([g[k]].*v>>1) + b
 }
-a[500] = c
-$><<a.join
+
+$><<a.join+c+b.join
