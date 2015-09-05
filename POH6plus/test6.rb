d43b3b26e15a7e0c0cd5956b412689b530ca7d94
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-06 05:31:07 +0900

    Update test6.rb
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/000f3ff1

--- POH6plus/test6.rb
+++ POH6plus/test6.rb
@@ -1,9 +1,18 @@
-f = Hash.new(0)
+f = Hash.new 0
+g = {}
 c = ""
-a = []
-ARGF.read.split[1..-1].map{|x|f[[x,y=x.reverse].min]+=1;x==y&&c=y}
-f.sort.map{|k,v|
-a+=[k].*v/2
+a = Array.new(1001, "")
+i = 0
+j = 1001
+ARGF.read.split[1..-1].map{|x|
+    f[[x,y=x.reverse].min] += 1;
+    g[x] = y
+    x==y&&c=y
 }
-s=a.join
-$><<s+c+s.reverse
+f.sort.map{|k,v|
+a.fill(k, i, v>>1)
+a.fill(g[k], j -= v>>1, v>>1)
+i+=v>>1
+}
+a[500] = c
+$><<a.join
