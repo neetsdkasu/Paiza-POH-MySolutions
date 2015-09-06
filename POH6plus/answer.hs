4070c3e20f476433ea57428a13a6431a163434c9
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-07 02:01:40 +0900

    バグがあったので修正。その他、名前の変更等
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/5200f10c

--- POH6plus/answer.hs
+++ POH6plus/answer.hs
@@ -1,31 +1,34 @@
+module Main where
+
+main :: IO ()
 main = do n <- readLn
-          w <- getWords n
-          printResult (resolve (strListSort w) [] [] [])
+          words <- getWords n
+          printResult (resolve (strListSort words) [] "" [])
 
 printResult :: [String] -> IO ()
 printResult [] = putStrLn ""
-printResult (hw:tw) = do putStr hw
-                         printResult tw
+printResult (words_head:words_tail) = do putStr words_head
+                                         printResult words_tail
 
-listHead :: [d] -> d
-listHead (h:t) = h
+listHead :: [anytype] -> anytype
+listHead (head:tail) = head
 
-listConcat :: [d] -> [d] -> [d]
+listConcat :: [anytype] -> [anytype] -> [anytype]
 listConcat [] [] = []
-listConcat a [] = a
-listConcat [] b = b
-listConcat [l] b = l : b
-listConcat (ha:ta) b = ha : listConcat ta b
+listConcat front [] = front
+listConcat [] end = end
+listConcat [element] end = element : end
+listConcat (front_head:front_tail) end = front_head : listConcat front_tail end
 
 getWords :: Int -> IO [String]
 getWords 0 = return []
-getWords n = do x <- getLine
-                y <- getWords (n - 1)
-                return (x:y)
+getWords n = do front <- getLine
+                end <- getWords (n - 1)
+                return (front : end)
 
 strReverse :: String -> String
 strReverse "" = ""
-strReverse (h:t) = listConcat (strReverse t) [h]
+strReverse (str_head:str_tail) = listConcat (strReverse str_tail) [str_head]
 
 strListReverse :: [String] -> [String]
 strListReverse [] = []
@@ -33,52 +36,52 @@ strListReverse (h:t) = listConcat (strListReverse t) [h]
 
 strCompare :: String -> String -> Int
 strCompare "" "" = 0
-strCompare a "" = 1
-strCompare "" b = -1
-strCompare (ha:ta) (hb:tb) = if ha < hb
-                                then -1
-                                else if ha == hb
-                                        then strCompare ta tb
-                                        else 1
+strCompare left "" = 1
+strCompare "" right = -1
+strCompare (left_head:left_tail) (right_head:right_tail) = if left_head < right_head
+                                                              then -1
+                                                              else if left_head == right_head
+                                                                      then strCompare left_tail right_tail
+                                                                      else 1
 
 underPartition :: String -> [String] -> [String]
-underPartition k [] = []
-underPartition k (hw:tw) = if strCompare k hw > 0
-                              then hw : (underPartition k tw)
-                              else underPartition k tw
+underPartition keyword [] = []
+underPartition keyword (words_head:words_tail) = if strCompare keyword words_head > 0
+                                                     then words_head : (underPartition keyword words_tail)
+                                                     else underPartition keyword words_tail
 
-samePartition :: String -> [String] -> [String]
-samePartition k [] = []
-samePartition k (hw:tw) = if strCompare k hw == 0
-                             then hw : (samePartition k tw)
-                             else samePartition k tw
+centerPartition :: String -> [String] -> [String]
+centerPartition keyword [] = []
+centerPartition keyword (words_head:words_tail) = if strCompare keyword words_head == 0
+                                                     then words_head : (centerPartition keyword words_tail)
+                                                     else centerPartition keyword words_tail
 
 upperPartition :: String -> [String] -> [String]
-upperPartition k [] = []
-upperPartition k (hw:tw) = if strCompare k hw < 0
-                              then hw : (upperPartition k tw)
-                              else upperPartition k tw
+upperPartition keyword [] = []
+upperPartition keyword (words_head:words_tail) = if strCompare keyword words_head < 0
+                                                    then words_head : (upperPartition keyword words_tail)
+                                                    else upperPartition keyword words_tail
 
 strListHead :: [String] -> String
 strListHead [] = ""
-strListHead (h:t) = h
+strListHead (str_head:str_tail) = str_head
 
 strListSort :: [String] -> [String]
 strListSort [] = []
-strListSort (h:t) = listConcat (listConcat (listConcat (strListSort (underPartition h t)) [h]) (strListSort (samePartition h t))) (strListSort (upperPartition h t))
+strListSort (h:t) = listConcat (listConcat (listConcat (strListSort (underPartition h t)) [h]) (strListSort (centerPartition h t))) (strListSort (upperPartition h t))
 
 findWord :: String -> [String] -> Bool
-findWord w [] = False
-findWord w (h:t) = if strCompare w h == 0 then True else findWord w t
+findWord keyword [] = False
+findWord keyword (words_head:words_tail) = if strCompare keyword words_head == 0 then True else findWord keyword words_tail
 
 deleteWord :: String -> [String] -> [String]
-deleteWord w [] = []
-deleteWord w (h:t) = if strCompare w h == 0 then t else h : deleteWord w t
+deleteWord keyword [] = []
+deleteWord keyword (words_head:words_tail) = if strCompare keyword words_head == 0 then words_tail else words_head : deleteWord keyword words_tail
 
-resolve :: [String] -> [String] -> [String] -> [String] -> [String]
-resolve [] b c d = listConcat (listConcat (strListReverse b) c) d
-resolve (k:a) b c d = if findWord (strReverse k) a 
-                         then resolve (deleteWord (strReverse k) a) (k : b) c ((strReverse k) : d)
-                         else if strCompare k (strReverse k) == 0
-                                 then resolve a b [k] d
-                                 else resolve a b c d                        
+resolve :: [String] -> [String] -> String -> [String] -> [String]
+resolve [] front center end = listConcat (listConcat (strListReverse front) [center]) end
+resolve (h:t) front center end = if findWord (strReverse h) t 
+                         then resolve (deleteWord (strReverse h) t) (h : front) center ((strReverse h) : end)
+                         else if strCompare h (strReverse h) == 0 && (strCompare "" center == 0 || strCompare h center < 0)
+                                 then resolve t front h end
+                                 else resolve t front center end
