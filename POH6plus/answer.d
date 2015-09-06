20c329af058b902ba25968e6fae3f463b3988ccd
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-07 05:38:47 +0900

    Update answer.d
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/64c0fe87

--- POH6plus/answer.d
+++ POH6plus/answer.d
@@ -1,78 +1,44 @@
 import std.stdio;
 import std.string;
-import std.bigint;
 
-string reverse(string str) {
-    string rev = "";
-    for (int i = 0; i < str.length; i++) {
-        rev = str[i] ~ rev;
-    }
-    return rev;
-}
-
-void main() {
+void main(){
+    int n;
     
-    string n_str = chomp(stdin.readln());
-    
-    if (n_str == null) {
-        return;
-    }
-    
-    if (isNumeric(n_str) == false) {
-        return;
-    }
-    
-    BigInt n_bigint = n_str;
-    
-    int n = n_bigint.toInt();
+    readf("%d\n", &n);
     
     string[] words;
     
     words.length = n;
     
-    for (auto i = 0; i < n; i++) {
-        string word = chomp(stdin.readln());
-        words[i] = word;
+    for (int i = 0; i < n; i++) {
+        words[i] = chomp(readln());
     }
     
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 1; j < n; j++) {
-            string word1 = words[j - 1];
-            string word2 = words[j];
-            if (word2 < word1) {
-                words[j - 1] = word2;
-                words[j] = word1;
-            }
-        }
-    }
+    words.sort;
     
-    string front = "";
-    string center = "";
-    string end = "";
+    string f = "", c = "", e = "";
     
     outer_loop:
     for (int i = 0; i < n; i++) {
-        if (words[i] == "") {
+        if (words[i] == "")
             continue;
+        string rev = "";
+        foreach (char ch ; words[i]) {
+            rev = ch ~ rev;
         }
-        string rev = reverse(words[i]);
         for (int j = i + 1; j < n; j++) {
-            if (words[j] == rev) {
-                front ~= words[i];
-                end = rev ~ end;
-                words[j] = "";
-                continue outer_loop;
-            }
+            if (words[j] != rev)
+                continue;
+            f ~= words[i];
+            e = rev ~ e;
+            words[j] = "";
+            continue outer_loop;
         }
-        if (words[i] == rev) {
-            if (center == "" || words[i] < center) {
-                center = words[i];
-            }
-        }
+        if (words[i] != rev)
+            continue;
+        if (c == "" || rev < c)
+            c = rev;
     }
     
-    string result = front ~ center ~ end;
-    
-    writeln(result);
-    
+    writeln(f ~ c ~ e);
 }
