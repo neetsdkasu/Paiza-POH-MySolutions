7504640ff13ff33981cbfead432f4e959789e9a6
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-06 23:02:18 +0900

    Update answer.hs
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/5c646be6

--- POH6plus/answer.hs
+++ POH6plus/answer.hs
@@ -1,8 +1,21 @@
-import Data.List
-
 main = do n <- readLn
           w <- getWords n
-          sequence_ (map putStr (resolve (sort w) [] [] []))
+          printResult (resolve (strListSort w) [] [] [])
+
+printResult :: [String] -> IO ()
+printResult [] = putStrLn ""
+printResult (hw:tw) = do putStr hw
+                         printResult tw
+
+listHead :: [d] -> d
+listHead (h:t) = h
+
+listConcat :: [d] -> [d] -> [d]
+listConcat [] [] = []
+listConcat a [] = a
+listConcat [] b = b
+listConcat [l] b = l : b
+listConcat (ha:ta) b = ha : listConcat ta b
 
 getWords :: Int -> IO [String]
 getWords 0 = return []
@@ -12,11 +25,11 @@ getWords n = do x <- getLine
 
 strReverse :: String -> String
 strReverse "" = ""
-strReverse (h:t) = (strReverse t) ++ [h]
+strReverse (h:t) = listConcat (strReverse t) [h]
 
 strListReverse :: [String] -> [String]
 strListReverse [] = []
-strListReverse (h:t) = (strListReverse t) ++ [h]
+strListReverse (h:t) = listConcat (strListReverse t) [h]
 
 strCompare :: String -> String -> Int
 strCompare "" "" = 0
@@ -28,6 +41,31 @@ strCompare (ha:ta) (hb:tb) = if ha < hb
                                         then strCompare ta tb
                                         else 1
 
+underPartition :: String -> [String] -> [String]
+underPartition k [] = []
+underPartition k (hw:tw) = if strCompare k hw > 0
+                              then hw : (underPartition k tw)
+                              else underPartition k tw
+
+samePartition :: String -> [String] -> [String]
+samePartition k [] = []
+samePartition k (hw:tw) = if strCompare k hw == 0
+                             then hw : (samePartition k tw)
+                             else samePartition k tw
+
+upperPartition :: String -> [String] -> [String]
+upperPartition k [] = []
+upperPartition k (hw:tw) = if strCompare k hw < 0
+                              then hw : (upperPartition k tw)
+                              else upperPartition k tw
+
+strListHead :: [String] -> String
+strListHead [] = ""
+strListHead (h:t) = h
+
+strListSort :: [String] -> [String]
+strListSort [] = []
+strListSort (h:t) = listConcat (listConcat (listConcat (strListSort (underPartition h t)) [h]) (strListSort (samePartition h t))) (strListSort (upperPartition h t))
 
 findWord :: String -> [String] -> Bool
 findWord w [] = False
@@ -38,7 +76,7 @@ deleteWord w [] = []
 deleteWord w (h:t) = if strCompare w h == 0 then t else h : deleteWord w t
 
 resolve :: [String] -> [String] -> [String] -> [String] -> [String]
-resolve [] b c d = (strListReverse b) ++ c ++ d
+resolve [] b c d = listConcat (listConcat (strListReverse b) c) d
 resolve (k:a) b c d = if findWord (strReverse k) a 
                          then resolve (deleteWord (strReverse k) a) (k : b) c ((strReverse k) : d)
                          else if strCompare k (strReverse k) == 0
