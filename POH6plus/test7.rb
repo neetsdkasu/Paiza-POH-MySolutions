7577ba66bea5f72e9c61db13560f998baa861d70
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-09 06:29:44 +0900

    Update test7.rb
    https://paiza.jp/poh/joshibato/matsue-ruby/result/d541c5c9

--- POH6plus/test7.rb
+++ POH6plus/test7.rb
@@ -1,1 +1,1 @@
-c="";a=readlines(())[k=0].split[1..-1].sort;s=(a.dup.map{|x|y=x.reverse;x==y&&(c=x)&&k+=1;a.index(y)&&(a-=[x])&&x}-[c]).join;$><<s+c*k+s.reverse
+c="";gets;a=readlines(())[k=0].split.sort;s=(a.dup.map{|x|y=x.reverse;x==y&&(c=x)&&k+=1;a.index(y)&&(a-=[x])&&x}-[c]).join;$><<s+c*k+s.reverse
