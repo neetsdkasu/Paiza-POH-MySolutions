16306872a1e667db77018188d0ed121636121a54
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-09 20:17:01 +0900

    Update test8b.rb
    result
    https://paiza.jp/poh/joshibato/matsue-ruby/result/9c49f8da

--- POH6plus/test8b.rb
+++ POH6plus/test8b.rb
@@ -1,1 +1,1 @@
-gets;w=ARGF.read.split.sort;c=s="";w.map{|x|!w.index(y=x.reverse)||y<x||(x==y&&c=x)||s+=x};$><<s+c*w.count(c)+s.reverse
+gets;w=ARGF.read.split.sort;c=s="";w.map{|x|!w.index(y=x.reverse)||(x==y&&c=x)||x<y&&s<<x};$><<s+c*w.count(c)+s.reverse
