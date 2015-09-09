9d8facd7f2408af2dc6eeb8082fc9f7436e440e3
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-09 17:05:20 +0900

    Update test5.rb
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/bb7d778f

--- POH6plus/test5.rb
+++ POH6plus/test5.rb
@@ -1,1 +1,1 @@
-f=Hash.new 0;s=c="";gets.to_i.times{f[[x=gets.chop,y=x.reverse].min]+=1;x==y&&c=x};f.sort.map{|k,v|s+=k.*v/2};$><<s+c+s.reverse
+f=Hash.new 0;s=c="";gets;(f[[x=$_.chop,y=x.reverse].min]+=1;x==y&&c=x)while gets;f.sort.map{|k,v|s+=k.*v/2};$><<s+c+s.reverse
