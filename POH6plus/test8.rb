447138b475ebe93edea722fa2e3a1da276380ffd
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-09 17:35:42 +0900

    Update test8.rb
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/8d1aeb03

--- POH6plus/test8.rb
+++ POH6plus/test8.rb
@@ -1,1 +1,1 @@
-gets;w=ARGF.read.split.sort;c="";k=0;s=w.reject{|x|y=x.reverse;(x==y&&(c=x)&&k+=1)||!w.index(y)||x>y}.join;$><<s+c*k+s.reverse
+gets;w=ARGF.read.split.sort;c=s="";k=0;w.map{|x|y=x.reverse;(x==y&&(c=x)&&k+=1)||!w.index(y)||y<x||s<<x};$><<s+c*k+s.reverse
