f82c9b9d9ae4cc182649da4b0705b1c9f8c15534
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-12 02:44:04 +0900

    Update test10.rb
    result
    https://paiza.jp/poh/joshibato/matsue-ruby/result/b5687dfe

--- POH6plus/test10.rb
+++ POH6plus/test10.rb
@@ -1,1 +1,1 @@
-n=gets.to_i;w=ARGF.read.split.sort;(n%6==1&&$><<w.join)||(s=w.map{|x|(w.index(y=x.reverse)&&x<y&&x)||""}.join;puts s+(n<9?"qaq":"")+s.reverse)
+n=gets.to_i;w=ARGF.read.split.sort;s=w.map{|x|(w.index(y=x.reverse)&&x<y&&x)||""}.join;$><<(n%6==1?w.join: s+(n<9?"qaq":"")+s.reverse)
