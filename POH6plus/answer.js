97d4262cc507d06619a80f98585acb1548654bea
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-12 23:30:42 +0900

    整理した
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/d1e5a6d8

--- POH6plus/answer.js
+++ POH6plus/answer.js
@@ -1,1 +1,77 @@
-process.stdin.resume();process.stdin.setEncoding('utf8');function z(s){return s.split("").reverse().join("");}process.stdin.on('data',function(a){var f=0;var e=a.indexOf("\n");var n=parseInt(a.substring(f,e));f=e+1;e=a.indexOf("\n",f);var l=e-f;e=a.indexOf("\n",e+1);var q=e-f-l;var i,b,d,r,c="",x,y,w;var u=0;x=a.substring(f,f+q);y=x;for(i=0;i<n;i++){e=f+i*q;d=a.substring(e,e+l);if(d<x)x=d;if(d>y)y=d;}b=x;for(;;) {r=z(b);if(b<=r){u=0;for(i=0;i<n;i++){e=f+i*q;d=a.substring(e,e+l);if(d===r)u++;}if(u>0){if(b===r){if(u%2==1){if(c===""||c>r)c=r;u--;}u/=2;}for(i=0;i<u;i++)process.stdout.write(b);}}if(b===y)break;w=b;b=y;for(i=0;i<n;i++){e=f+i*q;d=a.substring(e,e+l);if(d>w&&d<b)b=d;}}process.stdout.write(c);b=y;for(;;){r=z(b);if(b<=r){u=0;for(i=0;i<n;i++){e=f+i*q;d=a.substring(e,e+l);if(d===r)u++;}if(u>0){if(b===r){if(u%2==1)u--;u/=2;}for(i=0;i<u;i++){process.stdout.write(r);}}}if(b===x)break;w=b;b=x;for(i=0;i<n;i++){e=f+i*q;d=a.substring(e,e+l);if(d<w&&d>b)b=d;}}});
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+m="\n";
+c="";
+o=process.stdout;
+function P(h,f){
+    r=s.split("").reverse().join("");
+    if(s<=r){
+        u=0;
+        v=1;
+        while((v=h.indexOf(r,v+1))>0)
+            u++;
+        if(u>0){
+            if(s===r){
+                if(u%2==1){
+                    if (c===""||c>r)
+                        c=r;
+                    u--;
+                }
+                u/=2;
+            }
+            for(i=0;i<u;i++)
+                o.write(f?s:r);
+        }
+    }
+}
+
+process.stdin.on('data',function(h){
+    j=0;
+    k=h.indexOf(m);
+    n=h.substring(j,k)-0;
+    j=k+1;
+    k=h.indexOf(m,j);
+    l=k-j;
+    k=h.indexOf(m,k+1);
+    q=k-j-l;
+    y=h.substring(j,j+q);
+    x=y;
+    for(i=0;i<n;i++){
+        k=j+i*q;
+        t=h.substring(k,k+l);
+        if(t<y) 
+            y=t;
+        if(t>x) 
+            x=t;
+    }
+    s=y;
+    for(;;){
+        P(h,1);
+        if(s===x) 
+            break;
+        r=s;
+        s=x;
+        for(i=0;i<n;i++){
+            k=j+i*q;
+            t=h.substring(k,k+l);
+            if(t>r&&t<s) 
+                s=t;
+        }
+    }
+    o.write(c);
+    s=x;
+    for(;;){
+        P(h,0);
+        if(s===y)
+            break;
+        r=s;
+        s=y;
+        for(i=0;i<n;i++){
+            k=j+i*q;
+            t=h.substring(k,k+l);
+            if(t>s&&t<r)
+                s=t;
+            
+        }
+    }
+});
