bf1804a72d6d3599300dd62a9454ba149e82fbc8
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-12 20:11:21 +0900

    テストケース４が通らない。要原因究明
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/e8357c6b

--- POH6plus/answer.js
+++ POH6plus/answer.js
@@ -4,7 +4,7 @@ process.stdin.on('data', function (chunk) {
     function strrev(s) { return s.split("").reverse().join(""); }
     var w = chunk.toString().trim().split('\n');
     var n = parseInt(w[0]);
-    var s = "", c = "";
+    var c = "";
     for (var i = 1; i <= n; i++) {
         if (w[i] === "") {
             continue;
@@ -12,11 +12,20 @@ process.stdin.on('data', function (chunk) {
         var r = strrev(w[i]);
         var j = w.indexOf(r, i + 1);
         if (j > 0 && j != i) {
-            s += w[i];
+            process.stdout.write(w[i]);
             w[j] = "";
-        } else if (w[i] === r && (c === "" || c > r)) {
-            c = r;
+        } else {
+            if (w[i] === r && (c === "" || c > r)) {
+                c = r;
+            }
+            w[i] = "";
         }
     }
-    console.log(s+c+strrev(s));
+    process.stdout.write(c);
+    for (var k = n; k > 0; k--) {
+        if (w[k] === "") {
+            continue;
+        }
+        process.stdout.write(strrev(w[k]));
+    }
 });
