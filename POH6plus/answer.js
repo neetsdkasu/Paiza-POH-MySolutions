817dc966dc891106b0133e870fd4161b4d39e9bc
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-13 18:41:53 +0900

    俺が馬鹿だった
    最初のコードで通らなかったのは配列のソートを忘れていたからだったｗ
    result
    https://paiza.jp/poh/joshibato/matsue-ruby/result/1588f7a0

--- POH6plus/answer.js
+++ POH6plus/answer.js
@@ -1,1 +1,23 @@
-process.stdin.resume();process.stdin.setEncoding('utf8');m="\n";c="";o=process.stdout;function P(h,f){r=s.split("").reverse().join("");if(s<=r){u=0;v=1;while((v=h.indexOf(r,v+1))>0)u++;if(u>0){if(s===r){if(u%2==1){if (c===""||c>r)c=r;u--;}u/=2;}for(i=0;i<u;i++)o.write(f?s:r);}}}process.stdin.on('data',function(h){j=0;k=h.indexOf(m);n=h.substring(j,k)-0;j=k+1;k=h.indexOf(m,j);l=k-j;k=h.indexOf(m,k+1);q=k-j-l;y=h.substring(j,j+q);x=y;for(i=0;i<n;i++){k=j+i*q;t=h.substring(k,k+l);if(t<y) y=t;if(t>x) x=t;}s=y;for(;;){P(h,1);if(s===x) break;r=s;s=x;for(i=0;i<n;i++){k=j+i*q;t=h.substring(k,k+l);if(t>r&&t<s) s=t;}}o.write(c);s=x;for(;;){P(h,0);if(s===y)break;r=s;s=y;for(i=0;i<n;i++){k=j+i*q;t=h.substring(k,k+l);if(t>s&&t<r)s=t;}}});
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (chunk) {
+    function strrev(s) { return s.split("").reverse().join(""); }
+    var w = chunk.toString().trim().split('\n');
+    var n = parseInt(w[0]);
+    var s = "", c = "";
+    w.sort();
+    for (var i = 1; i <= n; i++) {
+        if (w[i] === "") {
+            continue;
+        }
+        var r = strrev(w[i]);
+        var j = w.indexOf(r, i + 1);
+        if (j > 0 && j != i) {
+            s += w[i];
+            w[j] = "";
+        } else if (w[i] === r && (c === "" || c > r)) {
+            c = r;
+        }
+    }
+    console.log(s+c+strrev(s));
+});
