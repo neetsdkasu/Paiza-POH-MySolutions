e371f6c11414ff17fb4642dbd36ca3a9bc8c0753
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-16 06:09:46 +0900

    Update test4.java
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/b074d5d9

--- POH6plus/test4.java
+++ POH6plus/test4.java
@@ -3,7 +3,6 @@ class Main {
     public static void main(String[]a)throws Exception {
         byte[]b=new byte[20000];System.in.read(b);
         String[]w=new String(b).split("\\s");
-        StringBuilder x=new StringBuilder(6000);
         int n=Integer.parseInt(w[0]),i,j;
         java.io.PrintStream p=System.out;
         switch(n){
@@ -11,8 +10,9 @@ class Main {
         case 1: p.print(w[1]);break;
         case 7: while(n-->0)p.print(w[1]);break;
         default:
+            StringBuilder x=new StringBuilder(6000);
             Arrays.sort(w,1,n+1);
-            Set<String>t=new HashSet<>();
+            Set<String>t=new HashSet<>(6000);
             for(i=1;i<=n;i++)t.add(w[i]);
             for(i=1;i<=n;i++){
                 String v=w[i],r=new StringBuilder(v).reverse().toString();
