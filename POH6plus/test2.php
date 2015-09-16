18f289ec89b8e12aab0dcb7720790acc80e9ad2b
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-16 22:33:33 +0900

    Create test2.php
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/8ac87dea

---
+++ POH6plus/test2.php
@@ -0,0 +1,1 @@
+<?php $w=[];for($i=fgets(STDIN)-0;$i-->0;$i){$x=trim(fgets(STDIN));if(isset($w[$x]))$w[$x]++;else$w[$x]=1;}ksort($w);$s=$c='';foreach($w as$k=>&$v){$r=strrev($k);if(!empty($w[$r])){$m=min($v,$w[$r]);$v=0;if($k==$r){$c=$k;$m>>=1;}$s.=str_repeat($k,$m);}}echo$s.$c.strrev($s);
