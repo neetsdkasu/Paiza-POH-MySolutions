3c4ddccbaf3777105ff1c7005bc0623c8f28dd4d
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-16 21:16:16 +0900

    Update test1.fsx
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/86de33a6

--- POH6plus/test1.fsx
+++ POH6plus/test1.fsx
@@ -1,8 +1,9 @@
 open System
 let w=List.sort [for i in 1..Int32.Parse(Console.ReadLine())->Console.ReadLine()]
-let rec f (x:List<string>) y z c =
-    match x with
-    |[]->printf "%s%s%s" y c z 
-    |h::t->let r=new string(Array.rev (h.ToCharArray()))
-           if List.exists (fun e->e=r) t&&h<r then f t (y+h) (r+z) c;elif r=h then f t y z (c+h);else f t y z c
+let rec f (x:List<string>)y z c=
+ match x with
+ |[]->printf"%s%s%s"y c z
+ |h::t->
+  let r=new string(Array.rev (h.ToCharArray()))
+  if List.exists (fun e->e=r)t&&h<r then f t (y+h)(r+z)c;elif r=h then f t y z (c+h);else f t y z c
 f w "" "" ""
