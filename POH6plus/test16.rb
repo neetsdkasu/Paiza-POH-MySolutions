092ede58dbcf5d22294bfad7391eccd5bcbbd0f2
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-16 19:33:18 +0900

    Update test16.rb
    result
    https://paiza.jp/poh/joshibato/matsue-ruby/result/bbf15d24

--- POH6plus/test16.rb
+++ POH6plus/test16.rb
@@ -1,1 +1,1 @@
-gets;w=ARGF.read.split.sort;v=w-(w-w.map{|x|x.reverse});u=v.select{|x|x<x.reverse};s=u*"";$><<s+v.select{|x|x==x.reverse}*""+s.reverse
+gets;w=ARGF.read.split.sort;c="";v=w-(w-w.map{|x|c+=x if x==y=x.reverse;y});u=v.select{|x|x<x.reverse};s=u*"";$><<s+c+s.reverse
