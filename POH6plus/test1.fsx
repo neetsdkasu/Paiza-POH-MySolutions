554806ee80989637b2bf4de54a0118d1fecf5504
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-16 20:58:28 +0900

    Update test1.fsx
    result;
    https://paiza.jp/poh/joshibato/matsue-ruby/result/468f5564

--- POH6plus/test1.fsx
+++ POH6plus/test1.fsx
@@ -2,20 +2,16 @@ open System
 
 let w = List.sort [for i in 1 .. Int32.Parse(Console.ReadLine()) -> Console.ReadLine()]
 
-let rec f (x:List<string>, y:string, z:string):Unit =
+let rec f (x:List<string>, y:string, z:string, c:string):Unit =
     if x.Length = 0 then
-        printf "%s%s" y z 
+        printf "%s%s%s" y c z 
     else
         let r = new string(Array.rev (x.Head.ToCharArray()))
         if List.exists (fun e -> e = r) x.Tail && x.Head < r then
-            f(x.Tail, y+x.Head, r + z)
+            f(x.Tail, y+x.Head, r + z, c)
+        elif r = x.Head then
+            f(x.Tail, y, z, c + x.Head)
         else
-            f(x.Tail, y, z)
+            f(x.Tail, y, z, c)
 
-match w.Length with
-| 6 -> printf "fdkjnvqaqvnjkdf"
-| 1 -> printf "%s" w.[0]
-| 7 -> [for i in 1 .. 7 -> printf "%s" w.[0]]; printf ""
-| q -> f(w, "", "")
-    
-    
+f(w,"","","")
