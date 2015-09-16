a9fc41ae36c1ccc68436c00a12942aa6747f417e
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-16 18:57:03 +0900

    Update test14.rb
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/d1cb4c09

--- POH6plus/test14.rb
+++ POH6plus/test14.rb
@@ -1,1 +1,1 @@
-gets;c="";w=ARGF.read.split.sort;s=w.reject{|x|c+=x if x==y=x.reverse;x>=y||!w.index(y)}.join;$><<s+c+s.reverse
+gets;c="";w=ARGF.read.split.sort;s=w.reject{|x|c+=x if x==y=x.reverse;x>=y||!w.index(y)}*"";$><<s+c+s.reverse
