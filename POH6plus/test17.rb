c45058f434895fcc01bc650cb713b8442168ae22
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-17 23:42:27 +0900

    Update test17.rb
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/27c08720

--- POH6plus/test17.rb
+++ POH6plus/test17.rb
@@ -1,1 +1,1 @@
-gets;$a=ARGF.read.split.sort;$s=$c="";def f;($a.size<1&&$><<$s+$c+$s.reverse)||(h=$a.shift;r=h.reverse;(h==r&&$c+=h)||$a.index(r)&&h<r&&$s+=h;f);end;f
+gets;def f(a,s);(a.size<1&&$><<s+$c+s.reverse)||(h=a.shift;r=h.reverse;(h==r&&$c+=h)||a.index(r)&&h<r&&s+=h;f(a,s));end;f(ARGF.read.split.sort,$c="")
