e51c17062f14f258156b70f7f8bad26a52372efc
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-18 03:31:44 +0900

    Update test18.rb
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/28db23d3

--- POH6plus/test18.rb
+++ POH6plus/test18.rb
@@ -1,14 +1,6 @@
-s=""
+s=c=""
 gets
-ARGF.read.split.group_by{|x|[x,x.reverse].min}.sort.map{|k,x|
-eval [0,'$><<k if k==k.reverse','$><<k;s=x.max+s',3,4,5,6,'s=k*7'][x.size]
-# case x.size
-# when 2
-#   $><<k; s=x.max+s
-# when 7
-#   s=k*7
-# else
-#   $><<k if k==k.reverse
-# end
+ARGF.read.split.group_by{|x|(x==y=x.reverse)&&c=x;[x,y].min}.sort.map{|k,x|
+(0<z=x.size/2)&&($><<k*z;s=x.max*z+s)
 }
-$><<s
+$><<c+s
