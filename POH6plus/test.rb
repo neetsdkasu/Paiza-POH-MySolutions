6ab90ca7318c0dafcfe2eaf4856a84c4e3ffa392
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-23 06:25:35 +0900

    能無しコード
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/08e87a7c

---
+++ POH6plus/test.rb
@@ -0,0 +1,1 @@
+exec 'perl -e \'$n=int(<STDIN>);@a=();while(<>){chomp($_);push @a,$_;}@a=sort(@a);$w=join(" ",@a);$c="";for$k(@a){$r=reverse($k);if($k lt$r&&$w=~/$r/){$w=~s/$r//;}else{$c.=$k if$k eq$r;$w=~s/$k//;}}$w=~s/ //g;print $w.$c.reverse($w);\''
