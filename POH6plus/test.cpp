136ac4e661396c05654a7995bc8e5fb957855bf6
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-23 03:40:40 +0900

    どアホなコード (Perl ver.)
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/e1b5fc76

--- POH6plus/test.cpp
+++ POH6plus/test.cpp
@@ -1,2 +1,2 @@
 #include<cstdlib>
-int main(){exit(system("ruby -e 'gets;w=ARGF.read.split.sort;c=s=\"\";w.map{|x|w.index(y=x.reverse)&&x<y&&s+=x;x==y&&c=x};$><<s+c*w.count(c)+s.reverse'"));}
+int main(){exit(system("perl -e '$n=int(<STDIN>);@a=();while(<>){chomp($_);push @a,$_;}@a=sort(@a);$w=join(\" \",@a);$c=\"\";for$k(@a){$r=reverse($k);if($k lt$r&&$w=~/$r/){$w=~s/$r//;}else{$c.=$k if$k eq$r;$w=~s/$k//;}}$w=~s/ //g;print $w.$c.reverse($w);'"));}
