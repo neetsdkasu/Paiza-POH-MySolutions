b5e0557818af17cbaca61fc8b9ad2e4548a32914
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-25 05:38:13 +0900

    どアホなコード (Perl ver.)その2
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/6147caa2

--- POH6plus/test.cpp
+++ POH6plus/test.cpp
@@ -1,2 +1,2 @@
 #include<cstdlib>
-int main(){exit(system("perl -e '$n=int(<STDIN>);@a=();while(<>){chomp($_);push @a,$_;}@a=sort(@a);$w=join(\" \",@a);$c=\"\";for$k(@a){$r=reverse($k);if($k lt$r&&$w=~/$r/){$w=~s/$r//;}else{$c.=$k if$k eq$r;$w=~s/$k//;}}$w=~s/ //g;print $w.$c.reverse($w);'"));}
+int main(){exit(system("perl -e '<>;%h=();$c=$s=\"\";while(<>){chop($_);@h{$_}=@h{$_}?@h{$_}+1:1;}for(sort(keys%h)){$r=reverse$_;if($r eq$_){$c.=$_ x@h{$_};}if(@h{$r}&&$_ lt$r){$s.=$_;}}print$s.$c.reverse($s);'"));}
