d5f4b03c6226dcbdbcf5ea2a74fc41526c181c12
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-09-25 19:42:19 +0900

    Create ruby.rb
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/d81e2c25

---
+++ POH6plus/array/ruby.rb
@@ -0,0 +1,47 @@
+#! ruby
+
+n = gets.to_i
+
+word_list = $<.read.split.sort
+
+left_side_palindrome = ""
+center_of_palindrome = ""
+right_side_palindrome = ""
+
+word_list.each_index do |i|
+    
+    word = word_list[i]
+    
+    if word == "" then
+        next
+    end
+    
+    word_list[i] = ""
+    
+    rev_word = word.reverse
+    
+    rev_index = word_list.index(rev_word)
+    
+    if rev_index.nil? || rev_index == i then
+        
+        if  word == rev_word && (center_of_palindrome == "" || word < center_of_palindrome) then
+    
+            center_of_palindrome = word
+            
+        end
+    
+    elsif rev_index > i then
+        
+        left_side_palindrome << word
+        
+        right_side_palindrome = rev_word + right_side_palindrome
+        
+        word_list[rev_index] = ""
+    
+    end
+    
+end
+
+palindrome = left_side_palindrome + center_of_palindrome + right_side_palindrome
+
+puts palindrome
