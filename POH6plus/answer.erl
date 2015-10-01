0a2c33b4402c7c34a4ceeb565028ecfbe3374baa
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-02 06:17:06 +0900

    1発で全テストケース通ったけど、糞重いｗ
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/07512314

--- POH6plus/answer.erl
+++ POH6plus/answer.erl
@@ -2,10 +2,26 @@
 -export([main/1]).
 
 main([_]) ->
-    N = io:get_line(""),
-    W = lists:sort(getlines()),
-    io:format("~s",[W]),
+    [{N}|W] = lists:sort(getlines()),
+    io:format("~s",[solve(W)]),
     init:stop().
+
+solve(S) -> solve(S,"","").
+solve([],L,C) ->
+    L ++ C ++ lists:reverse(L);
+solve([{H}|T],L,C) ->
+    R = lists:reverse(H),
+    case lists:keyfind(R,1,T) of
+        {X} ->
+            solve(lists:keydelete(X,1,T),L++H,C);
+        false ->
+            if
+                (H == R) and ((C == "") or (H < C)) ->
+                    solve(T,L,H);
+                true ->
+                    solve(T,L,C)
+            end
+    end.
 
 %% 複数行取得、文字列連結にならんようにタプルで囲む
 getlines() -> getlines([]).
