52cd9a5d4ef2edff13ad5e2e308d1e9bb421536f
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-02 05:27:41 +0900

    listsモジュール(?)にreverseが普通にあったｗ、ついでにsortも発見

--- POH6plus/answer.erl
+++ POH6plus/answer.erl
@@ -3,18 +3,16 @@
 
 main([_]) ->
     N = io:get_line(""),
-    [{W}|T] = getlines(),
-    io:format("~s",[reverse(W)]),
+    [{W}|T] = lists:sort(getlines()),
+    io:format("~s",[lists:reverse(W)]),
     init:stop().
 
-reverse(S) -> reverse(S,[]).
-reverse([],R) -> R;
-reverse([H|T],R) -> reverse(T,[H|R]).
-    
 
+%% chompっていうか右端1文字削り
 chomp([X|[]]) -> [];
 chomp([H|T]) -> [H|chomp(T)].
 
+%% 複数行取得、文字列連結にならんようにタプルで囲む
 getlines() -> getlines([]).
 getlines(Result) ->
     case io:get_line("") of
