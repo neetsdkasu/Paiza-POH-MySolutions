9c9a454ca810e0d9d8b4d7d23ee982fd6f3c1ea3
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-03 02:31:11 +0900

    コメント削除＆てきとーに隙間削除
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/0dc7c496

--- POH6plus/answer.go
+++ POH6plus/answer.go
@@ -1,61 +1,26 @@
 package main
 
-import (
-    "fmt"
-    "sort"
-)
+import("fmt";"sort")
 
-// How to Write Go Code の Reverse
-// https://golang.org/doc/code.html#Library
-func Reverse(s string) string {
-    r := []rune(s)
-    for i,j :=0,len(r)-1; i<=j; i,j=i+1,j-1 {
-        r[i],r[j]=r[j],r[i]
-    }
+func Reverse(s string)string{
+    r:=[]rune(s)
+    for i,j:=0,len(r)-1;i<=j;i,j=i+1,j-1 {r[i],r[j]=r[j],r[i]}
     return string(r)
 }
 
 func main(){
-    
-    var n int
-    fmt.Scanf("%d", &n)
-    // fmt.Printf("%d", n)
-    
-    // var w [1000]string // 静的？
-    // var w []string = make([]string, n) // 動的？
-    // w := make([]string, n) // 動的？
-    w := make(sort.StringSlice, n) // Sortメソッド等を持ってる拡張された[]string
-    for i := 0; i < n; i++ {
-        fmt.Scanf("%s", &w[i])
+    n:=0
+    fmt.Scanf("%d",&n)
+    w:=make([]string,n)
+    for i:=0;i<n;i++{fmt.Scanf("%s",&w[i])}
+    sort.Strings(w)
+    lp,rp,cp:="","",""
+    for i:=0;i<n;i++{
+        x:=w[i]
+        if x==""{continue}
+        r,j:=Reverse(x),i+1
+        for;j<n;j++{if r==w[j]{lp,rp,w[j]=lp+x,r+rp,"";break}}
+        if j==n&&x==r&&(cp==""||x<cp){cp=x}
     }
-    
-    // sort.Strings(w) // wに[]stringを使う場合
-    w.Sort() // StringSliceはSortメソッドを持っている
-    
-    lp := ""
-    rp := ""
-    cp := ""
-    
-    for i := 0; i < n; i++ {
-        x := w[i]
-        if x == "" {
-            continue
-        }
-        r := Reverse(x)
-        j := i + 1
-        for ; j < n; j++ {
-            if r == w[j] {
-                lp += x
-                rp = r + rp
-                w[j] = ""
-                break
-            }
-        }
-        if j == n && x == r && (cp == "" || x < cp) {
-            cp = x
-        }
-    }
-    
-    fmt.Println(lp + cp + rp)
-    
+    fmt.Print(lp+cp+rp)
 }
