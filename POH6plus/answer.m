c7a71a488c14d98e7029226ae7378e734b120c88
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-04 02:13:06 +0900

    残念ながら1発では通らなかった、修正した2度目で通った
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/2a06ba53

--- POH6plus/answer.m
+++ POH6plus/answer.m
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 
-NSComparisonResult comp(id o1, id o2, void* x) {
+NSComparisonResult comp(id o1, id o2, void* context) {
     return [(NSString*)o1 localizedCompare:(NSString*)o2];
 }
 
@@ -15,15 +15,45 @@ int main(void){
         NSMutableString *str = [NSMutableString alloc];
         [str initWithBytes:indata.bytes length:indata.length encoding:enc];
 
-        NSArray *w = [[str componentsSeparatedByString:@"\n"] sortedArrayUsingFunction:comp context:NULL];
-
+        NSMutableArray *w = [NSMutableArray new];
+        [w addObjectsFromArray:[[str componentsSeparatedByString:@"\n"] sortedArrayUsingFunction:comp context:NULL]];
+        
+        NSMutableString *cp = [NSMutableString new]; // 中央
+        NSMutableString *rp = [NSMutableString new]; // 右側
+        
+        // 最後の単語に改行があるので空文字と単語数Ｎがソート後の先頭2個に来るので省く
         for (NSUInteger j = 2; j < w.count; j++) {
             NSString *wd = (NSString*)[w objectAtIndex:j];
-            [stdout writeData:[wd dataUsingEncoding:enc]];
-            [stdout writeData:[@"----\n" dataUsingEncoding:enc]];
+            
+            if (wd.length == 0) {
+                continue;
+            }
+            
+            // 単語反転処理
+            NSMutableString *rd = [NSMutableString new];
+            for (NSUInteger x = 0; x < wd.length; x++) {
+                unichar ch = [wd characterAtIndex:(wd.length - x - 1)];
+                [rd appendFormat:@"%c", ch];
+            }
+            
+            NSRange range = NSMakeRange(j + 1, w.count - j - 1);
+            NSUInteger k = [w indexOfObject:rd inRange:range];
+            
+            if (k != NSNotFound) {
+                [stdout writeData:[wd dataUsingEncoding:enc]];
+                [rp insertString:rd atIndex:0];
+                [w replaceObjectAtIndex:k withObject:@""];
+            } else {
+                if ([wd localizedCompare:rd] == NSOrderedSame) {
+                    if (cp.length == 0 || [cp localizedCompare:wd] == NSOrderedDescending) {
+                        [cp setString:wd];
+                    }
+                }
+            }
         }
-
         
+        [rp insertString:cp atIndex:0];
+        [stdout writeData:[rp dataUsingEncoding:enc]];
     }
     
     return 0;
