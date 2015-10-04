c533cda572059e4fbfac153ee6b55080c54a5525
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-05 06:48:29 +0900

    改行、空白、インデント、削除
    result:
    https://paiza.jp/poh/joshibato/matsue-ruby/result/3a3a98d1

--- POH6plus/answer.hs
+++ POH6plus/answer.hs
@@ -1,10 +1,1 @@
-import Data.List
-
-main = getContents >>= \x ->
-       putStr $ solve "" "" "" $ sort $ tail $ lines x
-
-solve s c t [] = s ++ c ++ t
-solve s c t (x:xs)= let r = reverse x
-                     in case elemIndex r xs of
-                        (Just _) -> solve (s ++ x) c (r ++ t) (delete r xs)
-                        Nothing -> solve s (if x == r && (c == "" || x < c) then x else c) t xs
+import Data.List;main=getContents>>=putStr.v"""""".sort.tail.lines;v s c t[]=s++c++t;v s c t(x:xs)=let r=reverse x in case elemIndex r xs of{(Just _)->v(s++x)c(r++t)(delete r xs);Nothing->v s(if x==r&&(c== ""||x<c)then x else c) t xs}
