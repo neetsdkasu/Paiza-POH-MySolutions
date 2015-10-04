64efdae172e4c4aa640ffd584b6ebc2c1e775fbf
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-04 20:02:14 +0900

    書き途中、変数の参照の扱いがよくわからんね、整数変数の参照の$記号を除いた

--- POH6plus/answer.sh
+++ POH6plus/answer.sh
@@ -9,15 +9,15 @@ mapfile -t w
 for (( i=${w[0]}; i; i--)); do
     k=i
     for (( j=i-1; j; j--)); do
-        if [[ ${w[$j]} < ${w[$k]} ]]; then
+        if [[ ${w[j]} < ${w[k]} ]]; then
             k=j;
         fi;
     done
-    x=${w[$i]}
-    w[$i]=${w[$k]}
-    w[$k]=$x;
+    x=${w[i]}
+    w[i]=${w[k]}
+    w[k]=$x;
 done
 
 for (( i=${w[0]}; i; i--)); do
-    echo "${w[$i]}";
+    echo "${w[i]}";
 done
