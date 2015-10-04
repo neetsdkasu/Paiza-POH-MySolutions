fec2e0dc02be40b444aafea9ce8f93188e9832b2
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-04 19:49:37 +0900

    書き途中、ソートまで実装してみたが反転は無理じゃね？

--- POH6plus/answer.sh
+++ POH6plus/answer.sh
@@ -1,8 +1,21 @@
 declare -a w
-declare -i i
+declare -i i j k
+declare x
 
 mapfile -t w
 
-for ((i=${w[0]}; i; i--)) do
+for (( i=${w[0]}; i; i--)); do
+    k=i
+    for (( j=i-1; j; j--)); do
+        if [[ ${w[$j]} < ${w[$k]} ]]; then
+            k=j;
+        fi
+    done;
+    x=${w[$i]}
+    w[$i]=${w[$k]}
+    w[$k]=$x
+done
+
+for (( i=${w[0]}; i; i--)); do
     echo "${w[$i]}";
 done
