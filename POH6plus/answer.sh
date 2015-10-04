850d2b283b62378ab5612b00712b0f134e4401cc
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-05 01:38:48 +0900

    たぶん、これで合っているはず

--- POH6plus/answer.sh
+++ POH6plus/answer.sh
@@ -1,54 +1,72 @@
+#!/bin/bash
+
 function rev() {
-    len=${#1};
+    local -i len=${#1};
+    local -i ii;
     rw="";
     for (( ii=len; ii; ii--)); do
         rw+=${1:ii-1:1};
     done;
 };
 
-mapfile -t w;
+typeset -iA m;
+typeset -a w z;
+typeset -i n wn v i j k u;
+typeset x c s y;
+mapfile -t z;
 
-n=${w[0]};
-
-for (( i=n; i; i--)); do { 
-    k=$i;
-    for (( j=i-1; j; j--)); do { 
-        if [[ ${w[j]} < ${w[k]} ]]; then
-            k=$j;
-        fi;
-    } done;
-    x=${w[i]};
-    w[i]=${w[k]};
-    w[k]=$x;
-} done;
-
-s="";
+n=${z[0]};
 c="";
+s="";
 
 for (( i=n; i; i--)); do {
-    x=${w[i]};
-    if [[ $x == "" ]]; then
-        continue;
-    fi;
+    x=${z[i]};
     rev $x;
-    j=i-1;
-    for (( ; j; j--)); do { 
-        if [[ $rw == ${w[j]} ]]; then {
-            s+=$x;
-            w[j]="";
-            break;
-        } fi;
-    } done;
-    if [[ j -eq 0 ]]; then
+    if [[ $x < $rw ]]; then
+        y=$x;
+    else
+        y=$rw;
+    fi;
+    m[$y]+=1;
+}; done;
+
+wn=0;
+for x in ${!m[*]}; do { 
+    v=${m[$x]};
+    if [[ v -gt 1 ]]; then { 
+        w[$wn]=$x;
+        wn+=1;
+    }; fi;
+    u=$((v & 1));
+    if [[ u -eq 1 ]]; then { 
+        rev $x;
         if [[ $x == $rw ]]; then
-            if [[ $c == "" ]]; then
-                c=$x;
-            elif [[ $x < $c ]]; then
+            if [[ $c == "" || $c > $x ]]; then
                 c=$x;
             fi;
         fi;
-    fi;
-} done;
+    }; fi;
+}; done;
+
+for (( i=wn; i; i--)); do { 
+    k=$i;
+    for (( j=i-1; j; j--)); do {
+        if [[ ${w[j-1]} < ${w[k-1]} ]]; then
+            k=$j;
+        fi;
+    }; done;
+    x=${w[i-1]};
+    w[i-1]=${w[k-1]};
+    w[k-1]=$x;
+}; done;
+
+for (( i=wn; i; i--)); do {
+    x=${w[i-1]};
+    v=$((${m[$x]} >> 1));
+    for (( j=v; j; j--)); do {
+        s+=$x;
+    }; done;
+}; done;
 
 rev $s;
 
