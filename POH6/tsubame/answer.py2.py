dd1220b7a46e84e7859cfe11b2650cc7a21cb20e
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-05 16:46:54 +0900

    Create answer.py2.py

---
+++ POH6/tsubame/answer.py2.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+#
+# 結果 https://paiza.jp/poh/joshibato/tsubame/result/aa26ed7d
+#
+
+n_str = raw_input()
+
+n = int(n_str)
+n1 = int(n_str[1])
+n10 = int(n_str[0])
+
+r = n + n1 + n10
+
+print r
