3f98287064545f4aaa51577f1363b55f6a53f873
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-06 00:48:06 +0900

    Create answer.py3.py

---
+++ POH6/tsubame/answer.py3.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+#
+# 結果 https://paiza.jp/poh/joshibato/tsubame/result/e5907390
+#
+
+n_str = input()
+
+n = int(n_str)
+n1 = int(n_str[1:])
+n10 = int(n_str[:1])
+r = n + n1 + n10
+
+print(r)
