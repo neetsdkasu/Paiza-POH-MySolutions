29d2c4415c11252a523926ff331c197b2ccb47cb
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-05 23:56:53 +0900

    どうもACCEPTによる入力データの自動トリミング機能が末尾の改行の有無関係なく1文字削るらしい

--- POH6/tsubame/answer.cob
+++ POH6/tsubame/answer.cob
@@ -1,5 +1,5 @@
        *>
-       *> 結果 https://paiza.jp/poh/joshibato/tsubame/result/cbf8cf08
+       *> 結果 https://paiza.jp/poh/joshibato/tsubame/result/b4aa954c
        *>
        IDENTIFICATION DIVISION.
        PROGRAM-ID. Answer.
@@ -10,18 +10,15 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
               01 Answer-Values.
-                     05 x    PIC X(10).
-                     05 n    PIC 9(10).
-                     05 n1   PIC 9(10).
-                     05 n10  PIC 9(10).
-                     05 r    PIC 9(10) VALUE Is 0.
-                     05 r2   PIC 9(2).
-                     05 r3   PIC 9(3).
+                     05 n    PIC 9(3).
+                     05 n1   PIC 9(3).
+                     05 n10  PIC 9(3).
+                     05 r    PIC 9(3) VALUE Is 0.
+                     05 d    PIC Z(3).
        PROCEDURE DIVISION.
        MainRoutine SECTION.
        000-Main.
-              ACCEPT x.
-              MOVE x TO n.
+              ACCEPT n.
               MOVE n TO n10.
               DIVIDE 10 INTO n10.
               MULTIPLY 10 BY n10.
@@ -29,11 +26,6 @@
               SUBTRACT n10 FROM n1.
               DIVIDE 10 INTO n10.
               ADD n n1 n10 TO r.
-              IF NUMVAL(r) < 100 THEN
-                     MOVE NUMVAL(r) TO r2
-                     DISPLAY r2
-              ELSE
-                     MOVE NUMVAL(r) TO r3
-                     DISPLAY r3
-              END-IF.
+              MOVE r TO d.
+              DISPLAY TRIM(d).
        END PROGRAM Answer.
