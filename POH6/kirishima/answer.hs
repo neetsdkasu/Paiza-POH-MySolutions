c353b14e3a617c4f616babd0688d49d184e181e8
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-07 01:13:50 +0900

    Update answer.hs

--- POH6/kirishima/answer.hs
+++ POH6/kirishima/answer.hs
@@ -1,5 +1,5 @@
 --
--- 結果 https://paiza.jp/poh/joshibato/kirishima/result/94c6692f
+-- https://paiza.jp/poh/joshibato/kirishima/result/e45f9f5a
 --
 
 main = getContents >>= solve . map (\z -> read z :: Int) . words
@@ -7,14 +7,14 @@ main = getContents >>= solve . map (\z -> read z :: Int) . words
 solve (n:ns) = let (t, (_:d)) = splitAt n ns in follow t d
 
 follow t [] = return ()
-follow t (d:ds) = simulate d [] t >> follow t ds
+follow t (d:ds) = putStrLn (simulate d [] t) >> follow t ds
 
-simulate d [] _ | d < 1 = putStrLn "No"
+simulate d [] _ | d < 1 = "No"
 simulate d _ [0]
-    | d > 0 = putStrLn "No"
-    | otherwise = putStrLn "Yes"
+    | d > 0 = "No"
+    | otherwise = "Yes"
 simulate 0 tf (tb:tbs)
-    | tb == 0 = putStrLn "No"
+    | tb == 0 = "No"
     | otherwise = simulate tb tf (0:tbs)
 simulate d tf (tb:tbs) | d > 0 = simulate (d - 1) (tb:tf) tbs
 simulate d (tf:tfs) tb | d < 0 = simulate (d + 1) tfs (tf:tb)
