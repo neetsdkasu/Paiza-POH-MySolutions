b11a4f16aa6bc9edeffa1a3f60361fc3da1eaf68
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-07 21:33:58 +0900

    未提出だが、一応できた

--- POH6/rio/answer.sh
+++ POH6/rio/answer.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 
 typeset -a lines;
-typeset -i n i j task_ size_ wa wb ca cb gcd wca wcb ra rb;
-typeset x;
+typeset -i n i j task_ size_ wa wb ca cb gcd wca wcb ra rb ans;
 
 function split() {
     task_=$1;
@@ -50,20 +50,33 @@ function subBS() {
     rb=$(($rb / $gcd));
 };
 
+function mulBS() {
+    ra=$(($1 * $3));
+    rb=$(($2 * $4));
+    calcGCD $ra $rb;
+    ra=$(($ra / $gcd));
+    rb=$(($rb / $gcd));
+};
+
+function divBS() {
+    ra=$(($1 * $4));
+    rb=$(($2 * $3));
+    calcGCD $ra $rb;
+    ra=$(($ra / $gcd));
+    rb=$(($rb / $gcd));
+};
+
 mapfile -t lines;
 
 n=${lines[0]};
 
-wa=0
-wb=1
-ca=0
-cb=1
-
+wa=0;
+wb=1;
+ca=0;
+cb=1;
 
 for (( i=$n; $i; i--)); do {
     split ${lines[$n-$i+1]};
-    #echo $task_
-    #echo $size_
     case $task_ in
     1 ) {
             addBS $wa $wb $size_ 1;
@@ -79,9 +92,26 @@ for (( i=$n; $i; i--)); do {
             addBS $wa $wb $ca $cb;
             wca=$ra;
             wcb=$rb;
+            mulBS $size_ 1 $wa $wb;
+            divBS $ra $rb $wca $wcb;
+            subBS $wa $wb $ra $rb;
+            wa=$ra;
+            wb=$rb;
+            mulBS $size_ 1 $ca $cb;
+            divBS $ra $rb $wca $wcb;
+            subBS $ca $cb $ra $rb;
+            ca=$ra;
+            cb=$rb;
         } ;;
     esac;
 }; done;
 
-echo $wa
-echo $ca
+addBS $wa $wb $ca $cb;
+wca=$ra;
+wcb=$rb;
+mulBS 100 1 $ca $cb;
+divBS $ra $rb $wca $wcb;
+
+ans=$(($ra / $rb));
+
+echo $ans
