2ded9b9b5bf403ca30999b2059fb66f148a82653
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-09 02:17:05 +0900

    気になった部分をちょこっと修正

--- POH6/rio/answer.cob
+++ POH6/rio/answer.cob
@@ -1,5 +1,5 @@
        *>
-       *> 結果 https://paiza.jp/poh/joshibato/rio/result/3fa70112
+       *> 結果 https://paiza.jp/poh/joshibato/rio/result/2506c7df
        *>
        IDENTIFICATION DIVISION.
        PROGRAM-ID. Answer.
@@ -12,14 +12,14 @@
               01 Answer-Values.
                      05 n          PIC 9(2).  *> 05 である必要はなく 02 n PIC 9(2). でもよいらしい
                      05 i          PIC 9(2).
-                     05 w          PIC 9(11)V9(24) VALUE 0.0. *> 9(4)V9(20)は計算用 9(4).9(20)だと文字出力用？
+                     05 w          PIC 9(11)V9(24) VALUE 0.0. *> 9(11)V9(24)は計算用 9(11).9(24)だと文字出力用？
                      05 c          PIC 9(11)V9(24) VALUE 0.0. *> 最大36文字までらしい
                      05 wc         PIC 9(11)V9(24).
                      05 ans        PIC 9(11)V9(24).
                      05 dsp        PIC Z(10)9.        *> Zは表示用,9での上位桁の0をスペースに置き換える
                      05 tas.
-                            10 T   PIC 9(2). *> tが1桁と分かっているので文字数決め打ち分割
-                            10 S   PIC 9(3). *> sは3桁までなので
+                            10 t   PIC 9(2). *> tが1桁と分かっているので文字数決め打ち分割
+                            10 s   PIC 9(3). *> sは3桁までなので
        PROCEDURE DIVISION.
        MainRoutine SECTION.
        000-Main.
