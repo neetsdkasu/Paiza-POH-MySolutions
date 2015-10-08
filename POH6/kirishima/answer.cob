49ea8989b2e67545e40d23a295f2c0e5c3f1630e
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-10-09 06:29:40 +0900

    おい、BINARY-なんちゃらとか初めて知ったぞ！、つか文字列分割は意外に容易だぞ！

--- POH6/kirishima/answer.cob
+++ POH6/kirishima/answer.cob
@@ -10,13 +10,30 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
               01 Answer-Values.
-                     05 n      PIC 9(3).
-                     05 m      PIC 9(3).
-                     05 d      PIC 9(3).
-                     05 i      PIC 9(3).
+                     05 n      BINARY-CHAR.  *> PIC 9(4) とかより便利そうなのあるじゃん
+                     05 m      BINARY-CHAR.
+                     05 d      BINARY-SHORT.
+                     05 i      BINARY-SHORT.
+                     05 j      BINARY-SHORT.
+                     05 t_str  PIC X(1000).
+                     05 t_cnt  BINARY-SHORT.
+                     05 t_idx  BINARY-SHORT.
+                     05 t_table OCCURS 100 TIMES.
+                            10 t       BINARY-SHORT.
        PROCEDURE DIVISION.
        AnswerMain SECTION.
        000-Start.
+       
               ACCEPT n.
-              DISPLAY n.
+              ACCEPT t_str.
+              
+              *> 文字列の分割でけた
+              PERFORM WITH TEST AFTER VARYING i FROM 1 BY 1 UNTIL i = n
+                     UNSTRING t_str DELIMITED BY ALL SPACES INTO t(i) COUNT IN t_cnt
+                     DISPLAY t(i)
+                     ADD 2 TO t_cnt
+                     MOVE t_str(t_cnt:100) TO t_str
+              END-PERFORM.
+              
+              
        END PROGRAM Answer.
