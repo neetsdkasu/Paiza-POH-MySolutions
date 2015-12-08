719cd98038cdefb3fa9c3d3dd7f9e974c95692b6
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-08 23:16:58 +0900

    Create Answer.swift
    result:
    https://paiza.jp/poh/ando/share/f417f99b

---
+++ POH7/LongHair/Answer.swift
@@ -0,0 +1,16 @@
+// Try POH
+// author: Leonardone @ NEETSDKASU
+
+var y = 0
+
+while let s = readLine() {
+    if s == "yes" {
+        y += 1
+    }
+}
+
+if y >= 3 {
+    print("yes")
+} else {
+    print("no")
+}
