63f3725830dfb78e6e5aabd3f7ed9b42fa436f73
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-10 09:36:08 +0900

    Update CodeGolf.swift
    result:
    https://paiza.jp/poh/ando/share/e85c1419

--- POH7/Megane/CodeGolf.swift
+++ POH7/Megane/CodeGolf.swift
@@ -1,1 +1,1 @@
-var r = {readLine()!.utf8.filter{$0>32}},f = {Int(readLine()!)!},n = f(),q = r(),m=n;while --m>0{q+=r()};m=f();for _ in 1...m{q+=r()};for y in 0...n-m{l:for x in 0...n-m{for i in 0..<m{for j in 0..<m{if 0<q[(y+i)*n+x+j]^q[n*n+i*m+j]{continue l}}};print(y,x)}}
+var r = {readLine()!.utf8.filter{$0>32}},f = {Int(readLine()!)!},n = f(),q = r(),m=n;while --m>0{q+=r()};m=f();for _ in 1...m{q+=r()};for y in 0...n-m{l:for x in 0...n-m{for i in 0..<m*m{if 0<q[(y+i/m)*n+x+i%m]^q[n*n+i]{continue l}};print(y,x)}}
