c5ae21005672d21ec2dc372a3cbc79cb455b998b
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-12 07:36:53 +0900

    Update main.c
    result:
    https://paiza.jp/poh/ando/share/eae378ff

--- POH7/Megane/CodeGolf/main.c
+++ POH7/Megane/CodeGolf/main.c
@@ -1,4 +1,3 @@
 #include <stdio.h>
 #include <string.h>
-#define N 201
-char q[N][N],p[N][N];i,j,k,n,m,t;main(){scanf("%d\n",&n);for(i=0;i<n;)fgets(q[i++],N,stdin);scanf("%d\n",&m);for(i=0;i<m;)fgets(p[i++],N,stdin);for(i=0;i<=n-m;i++)for(j=0;j<=n-m;j++){t=0;for(k=0;k<m;k++)t+=!!memcmp(q[i+k]+j+j,p[k],m+m-1);if(!t)printf("%d %d\n",i,j);}}
+char q[100][201],p[10][21];i,j,k,n,m,t;main(){scanf("%d\n",&n);for(i=0;i<n;)fgets(q[i++],201,stdin);scanf("%d\n",&m);for(i=0;i<m;)fgets(p[i++],21,stdin);for(i=0;i<=n-m;i++)for(j=0;j<=n-m;j++){t=0;for(k=0;k<m;k++)t+=!!memcmp(q[i+k]+j+j,p[k],m+m-1);if(!t)printf("%d %d\n",i,j);}}
