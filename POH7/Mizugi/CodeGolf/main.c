279e6a765c605106f7bc6b1e397e573bfd6f584b
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-12 08:34:55 +0900

    Update main.c
    result:
    https://paiza.jp/poh/ando/share/810fbaa5

--- POH7/Mizugi/CodeGolf/main.c
+++ POH7/Mizugi/CodeGolf/main.c
@@ -1,1 +1,1 @@
-main(){long long n,i,r=1,b=0,m=1000000000;scanf("%d",&n);while(i=n--,n){for(;~i&1;i/=2)b++;for(;!(i%5);i/=5)b--;r=(r*i)%m;}while(b--)r=(r*2)%m;printf("%d",r);}
+long long n,i,r=1,b,m=1000000000;main(){scanf("%d",&n);for(;i=n--,n;r=r*i%m){for(;~i&1;i/=2)b++;for(;!(i%5);i/=5)b--;}while(b--)r=r*2%m;printf("%d",r);}
