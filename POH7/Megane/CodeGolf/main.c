4d411f739152b2f6a69f11a88cebc7a51fafe4e6
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-12 07:56:37 +0900

    Update main.c
    result:
    https://paiza.jp/poh/ando/share/f18b1902

--- POH7/Megane/CodeGolf/main.c
+++ POH7/Megane/CodeGolf/main.c
@@ -1,1 +1,1 @@
-char q[100][201],p[10][21],*s="%d %d\n";i,j,k,n,m,t;main(){scanf(s+3,&n);for(i=0;i<n;)gets(q+i++);scanf(s+3,&m);for(i=0;i<m;)gets(p+i++);for(i=0;i<=n-m;i++)for(j=0;j<=n-m;j++){t=0;for(k=m;k--;)t+=!!memcmp(q[i+k]+j+j,p+k,m+m-1);t||printf(s,i,j);}}
+char q[100][201],p[10][21],*s="%d %d\n";i,j,k,n,m,t;main(){scanf(s+3,&n);for(i=0;i<n;)gets(q+i++);scanf(s+3,&m);for(i=0;i<m;)gets(p+i++);for(i=n-m+1;i--;)for(j=0;j<=n-m;j++){t=0;for(k=m;k--;)t+=!!memcmp(q[i+k]+j+j,p+k,m+m-1);t||printf(s,i,j);}}
