30eda4af2d0a6d8ec372d95f571151f10a8dc2f2
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-13 00:57:43 +0900

    Update CodeGolf.swift
    result:
    https://paiza.jp/poh/ando/share/4f3fb45c

--- POH7/SantaFuku/CodeGolf.swift
+++ POH7/SantaFuku/CodeGolf.swift
@@ -1,1 +1,1 @@
-var r={readLine()!.utf8.split{$0==32}.map{Int(String($0))!}},a=r(),b=[0,a[0]],c=[0,a[1]];while --a[3]>=0{var d=r();if d[0]>0{c+=[d[1]]}else{b+=[d[1]]}};func m(x:[Int])->Int{return(1..<x.count).map{x[$0]-x[$0-1]}.minElement()!};print(a[2]*m(b.sort())*m(c.sort()))
+var r={readLine()!.utf8.split{$0==32}.map{Int(String($0))!}},a=r(),b=[0,a[0]],c=[0,a[1]],m={(x:[Int])->Int in(1..<x.count).map{x[$0]-x[$0-1]}.minElement()!};while --a[3]>=0{var d=r();if d[0]>0{c+=[d[1]]}else{b+=[d[1]]}};print(a[2]*m(b.sort())*m(c.sort()))
