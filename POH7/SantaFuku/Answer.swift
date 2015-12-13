d630599f320a113d567ed5e652af792f40135403
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-13 09:52:26 +0900

    Update Answer.swift
    result:
    https://paiza.jp/poh/ando/share/1a0824e0

--- POH7/SantaFuku/Answer.swift
+++ POH7/SantaFuku/Answer.swift
@@ -8,11 +8,9 @@ for _ in 1...xyzn[3]{
     let da=readInts()
     if da[0]==0{xs+=[da[1]]}else{ys+=[da[1]]}
 }
-xs.sortInPlace()
-ys.sortInPlace()
 func mn(a:[Int])->Int{
     var m = a[a.count-1]
     for i in 1..<a.count{m=min(m,a[i]-a[i-1])}
     return m
 }
-print(xyzn[2]*mn(xs)*mn(ys))
+print(xyzn[2]*mn(xs.sort())*mn(ys.sort()))
