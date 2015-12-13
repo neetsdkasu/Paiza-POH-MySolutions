2eabffc2408d04b3ed52914494cf53a7d6803a74
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-13 09:43:50 +0900

    Update Answer.swift
    result:
    https://paiza.jp/poh/ando/share/73644907

--- POH7/SantaFuku/Answer.swift
+++ POH7/SantaFuku/Answer.swift
@@ -1,40 +1,18 @@
 // Try POH
 // author: Leonardone @ NEETSDKASU
 
-func readInt() -> Int {
-    return Int(readLine() ?? "0") ?? 0
+var readInts={readLine()!.utf8.split{$0==32}.map{Int(String($0))!}}
+
+var b=readInts(),(x,y,z,n)=(b[0],b[1],b[2],b[3]),xs=[0,b[0]],ys=[0,b[1]]
+for _ in 1...b[3]{
+    let da=readInts()
+    if da[0]==0{xs+=[da[1]]}else{ys+=[da[1]]}
 }
-
-func readInts() -> [Int] {
-    return (readLine() ?? "0").characters.split{$0==" "}.map{String($0)}.map{Int($0) ?? 0}
-}
-
-let b = readInts(), (x,y,z,n)=(b[0],b[1],b[2],b[3])
-
-var para_soku = [0,x]
-var para_zen = [0,y]
-
-for _ in 1...n {
-    let da = readInts()
-    if da[0] == 0 {
-        para_soku.append(da[1])
-    } else {
-        para_zen.append(da[1])
-    }
-}
-
-para_soku.sortInPlace()
-para_zen.sortInPlace()
-
-func mn(a :[Int])->Int {
-    var m = a[a.count - 1]
-    for i in 1..<a.count {
-        m = min(m, a[i] - a[i-1])
-    }
+xs.sortInPlace()
+ys.sortInPlace()
+func mn(a:[Int])->Int{
+    var m = a[a.count-1]
+    for i in 1..<a.count{m=min(m,a[i]-a[i-1])}
     return m
 }
-
-print(z * mn(para_soku) * mn(para_zen))
-
-
-
+print(b[2]*mn(xs)*mn(ys))
