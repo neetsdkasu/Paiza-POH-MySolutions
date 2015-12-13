d4dfd398e86bbf391ad672e7c8cd9e5232fd10be
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-13 09:47:34 +0900

    Update Answer.swift
    result:
    https://paiza.jp/poh/ando/share/1ff22db9

--- POH7/SantaFuku/Answer.swift
+++ POH7/SantaFuku/Answer.swift
@@ -3,8 +3,8 @@
 
 var readInts={readLine()!.utf8.split{$0==32}.map{Int(String($0))!}}
 
-var b=readInts(),(x,y,z,n)=(b[0],b[1],b[2],b[3]),xs=[0,b[0]],ys=[0,b[1]]
-for _ in 1...b[3]{
+var xyzn=readInts(),xs=[0,xyzn[0]],ys=[0,xyzn[1]]
+for _ in 1...xyzn[3]{
     let da=readInts()
     if da[0]==0{xs+=[da[1]]}else{ys+=[da[1]]}
 }
@@ -15,4 +15,4 @@ func mn(a:[Int])->Int{
     for i in 1..<a.count{m=min(m,a[i]-a[i-1])}
     return m
 }
-print(b[2]*mn(xs)*mn(ys))
+print(xyzn[2]*mn(xs)*mn(ys))
