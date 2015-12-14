585432569ff1d867592adb1511c7dd3b13f4213d
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2015-12-14 17:50:06 +0900

    Create CodeGolf.swift
    result:
    https://paiza.jp/poh/ando/share/f743fd0a

---
+++ POH7/Gantai/CodeGolf.swift
@@ -0,0 +1,1 @@
+var r={readLine()!.utf8.split{$0==32}.map{Int(String($0))!}.sort()},n=r(),u=r(),x=r(),v=r(),y=r(),s="";for i in y{if !x.contains(i){s+=(s>"" ?" ":"")+String(i)}};print(s>"" ?s:"None")
