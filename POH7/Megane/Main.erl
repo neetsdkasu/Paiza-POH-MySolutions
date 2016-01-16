523831704d6f1315805b40b1c3ce036a3b52c1f6
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2016-01-17 03:57:21 +0900

    F#でやってて気付いたことをこちらにも反映させてみた
    result:
    https://paiza.jp/poh/ando/share/0c429c6a

--- POH7/Megane/Main.erl
+++ POH7/Megane/Main.erl
@@ -3,7 +3,7 @@
 -module(main).
 -export([main/1,ngs/1,ngi/1,ngss/1,ngis/1]).
 -import(io,[format/2]).
--import(lists,[sort/1,seq/2,foldl/3,sublist/3,flatten/1,split/2,nthtail/2,takewhile/2,flatmap/2]).
+-import(lists,[seq/2,sublist/3,nthtail/2,takewhile/2,map/2]).
 
 ti(S) -> {V,_} = string:to_integer(S), V.
 gs() -> io:get_line("").
@@ -20,20 +20,18 @@ main(_) ->
     N = gi(),
     P = ngis(N),
     M = gi(),
-    Q = flatten(ngis(M)),
-    T = length(takewhile(fun(E) -> E /= Q end, flatmap(fun(X)->X end, subpics(N,M,P)))),
+    Q = ngis(M),
+    T = length(takewhile(fun(E) -> E /= Q end, subpics(N,M,P))),
     NM = N - M + 1,
     Y = T div NM,
     X = T rem NM,
-    format("~w ~w\n",[Y, X]),
+    format("~w ~w\n",[Y,X]),
     halt().
 
 xparts(_,_,[]) -> [];
 xparts(M,P,[X|XS]) -> [cutpic(X,M,P)|xparts(M,P,XS)].
 
-subpics(N,M,P) when length(P) < M -> [];
-subpics(N,M,P) -> [xparts(M,P,seq(1,N-M+1))|subpics(N,M,nthtail(1,P))].
+subpics(_,M,P) when length(P) < M -> [];
+subpics(N,M,P) -> xparts(M,P,seq(1,N-M+1)) ++ subpics(N,M,nthtail(1,P)).
 
-cutpic(I,M,P) ->
-    {F,_} = split(M,P),
-    foldl(fun(Pt,R)->R ++ sublist(Pt,I,M) end,[],F).
+cutpic(I,M,P) -> map(fun(E) -> sublist(E,I,M) end, sublist(P,1,M)).
