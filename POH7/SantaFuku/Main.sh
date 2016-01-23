f343c94f5906339d8088a54b18dd754cb667a208
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2016-01-23 10:20:58 +0900

    バケットソートにしてやっと通った・・・
    result:
    https://paiza.jp/poh/ando/share/eaab0a43

--- POH7/SantaFuku/Main.sh
+++ POH7/SantaFuku/Main.sh
@@ -1,56 +1,45 @@
 #!/bin/bash
 # Try POH
 # author: Leonardone @ NEETSDKASU
-declare -i x y z n d a xc yc t i j k xmin ymin;
+declare -i x y z n d a t i j k xmin ymin;
 declare -ai xs ys;
 read x y z n;
 xmin=$x;
 ymin=$y;
-function min() { let $(($1=($1 < $2) ? $1 : $2)); };
-xs[0]=0;
-xs[1]=$x;
-ys[0]=0;
-ys[1]=$y;
-xc=2;
-yc=2;
-for ((;n;n--)); do #
+for ((i=101;i;i--)); do #
+    xs[i]=0;
+    ys[i]=0;
+done;
+xs[0]=1;
+xs[x]=1;
+ys[0]=1;
+ys[y]=1;
+for ((;n;n--)) do #
     read d a;
     if (( d == 0 )); then #
-        xs[xc]=$a;
-        let xc++;
+        xs[a]=1;
     else #
-        ys[yc]=$a;
-        ((yc++));
+        ys[a]=1;
     fi;
 done;
-for ((i=0;((i<xc));i++)); do #
-    j=$i;
-    t=${xs[$j]};
-    for ((k=i+1;((k<xc));k++)); do #
-        if ((t < xs[k])); then #
-            j=$k;
-            t=${xs[$k]};
+k=0;
+for ((i=1;i<101;i++)); do #
+    if ((xs[i])); then #
+        t=$((i - k));
+        if ((t < xmin)); then #
+            xmin=$t;
         fi;
-    done;
-    xs[j]=${xs[$i]};
-    xs[i]=$t;
+        k=$i;
+    fi;
 done;
-for ((i=1;((i<xc));i++)); do #
-    min xmin $((xs[i-1] - xs[i]));
-done;
-for ((i=0;((i<yc));i++)); do #
-    j=$i;
-    t=${ys[$j]};
-    for ((k=i+1;((k<yc));k++)); do #
-        if ((t < ys[k])); then #
-            j=$k;
-            t=${ys[$k]};
+k=0;
+for ((i=1;i<101;i++)); do #
+    if ((ys[i])); then #
+        t=$((i - k));
+        if ((t < ymin)); then #
+            ymin=$t;
         fi;
-    done;
-    ys[j]=${ys[$i]};
-    ys[i]=$t;
-done;
-for ((i=1;((i<yc));i++)); do #
-    min ymin $((ys[i-1] - ys[i]));
+        k=$i;
+    fi;
 done;
 echo $(( xmin * ymin * z ));
