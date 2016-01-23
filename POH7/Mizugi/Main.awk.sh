00ac8b4f99a0d593503966241c3e72ce1da0fd3b
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2016-01-24 06:25:03 +0900

    一行にまとめてみた。特に意味は無い。
    result:
    https://paiza.jp/poh/ando/share/0046a435

--- POH7/Mizugi/Main.awk.sh
+++ POH7/Mizugi/Main.awk.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 # Try POH
 # author: Leonardone @ NEETSDKASU
-
+#
 # よもやこれはBashじゃなくawk
-awk '{
-c=0;
-t=int($1/5);
-while(t>0){
-    c+=t;
-    t=int(t/5)
-};
-r=1;
-for(i=1;i<=$1;i++){
-    x=i;
-    while(x%5==0){
-        x=int(x/5)
-    };
-    if(c>0){
-        while(x%2==0){
-            x=int(x/2);
-            c--;
-            if(c==0){break}
-        }
-    };
-        r=(r*x)%1000000000;
-    };
-print r
-}'
+awk '{t=int($1/5);while(t>0){c+=t;t=int(t/5)};r=1;for(i=$1;x=i--;){while(x%5<1)x/=5;if(c>0)while(x%2<1){x/=2;if(--c<1)break}r=r*x%10^9}print r}';
