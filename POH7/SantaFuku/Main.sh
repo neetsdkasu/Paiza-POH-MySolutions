c70aefcb1d6b375babd19dd8775c1034909442af
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2016-01-23 09:54:32 +0900

    60点・・・
    result:
    https://paiza.jp/poh/ando/share/36dc15ad
    総当りで求める から ソートして求めるに変えたら
    paiza.ioやideoneでは速度上がったのにPOHでは相変わらず3秒・・・どうなってんだよ・・・

--- POH7/SantaFuku/Main.sh
+++ POH7/SantaFuku/Main.sh
@@ -1,41 +1,56 @@
 #!/bin/bash
 # Try POH
 # author: Leonardone @ NEETSDKASU
-declare -i x y z n d a xc yc t i xmin ymin;
+declare -i x y z n d a xc yc t i j k xmin ymin;
 declare -ai xs ys;
 read x y z n;
 xmin=$x;
 ymin=$y;
-function abs() { let $(($1=($1 < 0) ? -$1 : $1 )); };
 function min() { let $(($1=($1 < $2) ? $1 : $2)); };
-xc=0;
-yc=0;
-for ((;n;n--)); do { #
+xs[0]=0;
+xs[1]=$x;
+ys[0]=0;
+ys[1]=$y;
+xc=2;
+yc=2;
+for ((;n;n--)); do #
     read d a;
-    if (( d == 0 )); then { #
-        min xmin $a;
-        t=$(( x - a ));
-        abs t;
-        min xmin $t;
-        for ((i=0;i<xc;i++)); do { #
-            t=$(( xs[i] - a ));
-            abs t;
-            min xmin $t;
-        }; done;
+    if (( d == 0 )); then #
         xs[xc]=$a;
         let xc++;
-    }; else { #
-        min ymin $a;
-        t=$(( y - a ));
-        abs t;
-        min ymin $t;
-        for ((i=0;i<yc;i++)); do { #
-            t=$(( ys[i] - a ));
-            abs t;
-            min ymin $t;
-        }; done;
+    else #
         ys[yc]=$a;
         ((yc++));
-    }; fi;
-}; done;
+    fi;
+done;
+for ((i=0;((i<xc));i++)); do #
+    j=$i;
+    t=${xs[$j]};
+    for ((k=i+1;((k<xc));k++)); do #
+        if ((t < xs[k])); then #
+            j=$k;
+            t=${xs[$k]};
+        fi;
+    done;
+    xs[j]=${xs[$i]};
+    xs[i]=$t;
+done;
+for ((i=1;((i<xc));i++)); do #
+    min xmin $((xs[i-1] - xs[i]));
+done;
+for ((i=0;((i<yc));i++)); do #
+    j=$i;
+    t=${ys[$j]};
+    for ((k=i+1;((k<yc));k++)); do #
+        if ((t < ys[k])); then #
+            j=$k;
+            t=${ys[$k]};
+        fi;
+    done;
+    ys[j]=${ys[$i]};
+    ys[i]=$t;
+done;
+for ((i=1;((i<yc));i++)); do #
+    min ymin $((ys[i-1] - ys[i]));
+done;
 echo $(( xmin * ymin * z ));
