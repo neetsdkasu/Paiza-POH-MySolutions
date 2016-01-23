e291e930b3a9c6cedf20461f810eba71283af49f
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2016-01-24 06:35:45 +0900

    awkは初めて弄ったのでちょっとだけ遊んでみただけでした
    result:
    https://paiza.jp/poh/ando/share/7a591cc1

--- POH7/Mizugi/Main.awk.sh
+++ POH7/Mizugi/Main.awk.sh
@@ -3,4 +3,4 @@
 # author: Leonardone @ NEETSDKASU
 #
 # よもやこれはBashじゃなくawk
-awk '{t=$1;while(t=int(t/5))c+=t;r=1;for(i=$1;x=i--;){while(x%5<1)x/=5;while(x%2<1&&0<c--)x/=2;r=r*x%10^9}print r}';
+awk '{t=$1;while(t=int(t/5))c+=t;r=1;while(x=$1--){while(x%5<1)x/=5;while(x%2<1&&0<c--)x/=2;r=r*x%10^9}print r}';
