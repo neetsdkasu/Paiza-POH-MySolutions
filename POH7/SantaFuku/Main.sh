812f58f7c977e2151c79c9173b37f4936536b1cd
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2016-01-23 10:51:08 +0900

    さらに高速化した
    result:
    https://paiza.jp/poh/ando/share/101f50ec

--- POH7/SantaFuku/Main.sh
+++ POH7/SantaFuku/Main.sh
@@ -6,36 +6,30 @@ declare -ai xs ys;
 read x y z n;
 xmin=$x;
 ymin=$y;
-xs[0]=1;
-xs[x]=1;
-ys[0]=1;
-ys[y]=1;
+xs[x]=x;
+ys[y]=y;
 for ((;n;n--)) do #
     read d a;
     if (( d == 0 )); then #
-        xs[a]=1;
+        xs[a]=a;
     else #
-        ys[a]=1;
+        ys[a]=a;
     fi;
 done;
 k=0;
-for ((i=1;i<=x;i++)); do #
-    if ((xs[i])); then #
-        t=$((i - k));
-        if ((t < xmin)); then #
-            xmin=$t;
-        fi;
-        k=$i;
+for i in ${xs[@]}; do #
+    t=$((i - k));
+    if ((t < xmin)); then #
+        xmin=$t;
     fi;
+    k=$i;
 done;
 k=0;
-for ((i=1;i<=y;i++)); do #
-    if ((ys[i])); then #
-        t=$((i - k));
-        if ((t < ymin)); then #
-            ymin=$t;
-        fi;
-        k=$i;
+for i in ${ys[@]}; do #
+    t=$((i - k));
+    if ((t < ymin)); then #
+        ymin=$t;
     fi;
+    k=$i;
 done;
 echo $(( xmin * ymin * z ));
