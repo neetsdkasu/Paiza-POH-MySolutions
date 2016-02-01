fba2490f0f7904fa27f7bd0d94765fd0c3f95ff7
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2016-02-02 01:50:27 +0900

    まずそうな部分があったのでちょい修正。この方法はPOHでは使えるようだけどIDEONEとかだとエラー

--- POH6/tsubame/After-the-event.cob
+++ POH6/tsubame/After-the-event.cob
@@ -1,7 +1,7 @@
        *> Try POH
        *> author: Leonardone @ NEETSDKASU
        *>
-       *> 結果:  https://paiza.jp/poh/joshibato/tsubame/result/0c448c89
+       *> 結果: https://paiza.jp/poh/joshibato/tsubame/result/b873ea35
        *>
        IDENTIFICATION DIVISION.
        PROGRAM-ID. Main.
@@ -18,6 +18,7 @@
               05 r    PIC 9(3) VALUE Is 0.
               05 d    PIC Z(3).
        01 For-Inputing-Values.
+              05 file-access PIC X USAGE COMP-X VALUE IS 1.
               05 file-handle PIC X(4) USAGE COMP-X.
               05 file-offset PIC X(8) USAGE COMP-X VALUE IS 0.
               05 file-nbytes PIC X(4) USAGE COMP-X VALUE IS 2.
@@ -26,7 +27,7 @@
        MainRoutine SECTION.
        000-Main.
               *> ACCEPT n.
-              CALL "CBL_OPEN_FILE" USING "/dev/stdin" 1 0 0 file-handle.
+              CALL "CBL_OPEN_FILE" USING "/dev/stdin" file-access 0 0 file-handle.
               CALL "CBL_READ_FILE" USING file-handle file-offset file-nbytes 0 file-buffer.
               CALL "CBL_CLOSE_FILE" USING file-handle.      
               MOVE INTEGER(TRIM(file-buffer)) TO n.
