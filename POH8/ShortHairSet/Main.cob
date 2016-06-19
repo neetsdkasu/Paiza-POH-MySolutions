a98b2e9a2c4d8fedd881d9f90b18dcec639eb0fa
Leonardone @ NEETSDKASU <neetsdkasu@yahoo.co.uk>
2016-06-20 07:00:55 +0900

    大文字変更と改行加えた
    https://paiza.jp/poh/hatsukoi/share/74ded44a

--- POH8/ShortHairSet/Main.cob
+++ POH8/ShortHairSet/Main.cob
@@ -3,24 +3,27 @@
        *> ↓ 1行あたりの字数の目安
        *>==============================================================$
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. Main.
+       PROGRAM-ID. MAIN.
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        REPOSITORY.
               FUNCTION ALL INTRINSIC.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 Answer-Values.
+       01 ANSWER-VALUES.
               05 INT-N   PIC 9(3).
               05 STR-S   PIC X(20).
               05 INT-I   PIC 9(3).
        PROCEDURE DIVISION.
-       MainRoutine SECTION.
-       000-Main.
+       MAIN-ROUTINE SECTION.
+       000-MAIN.
+              
               ACCEPT INT-N.
               ACCEPT STR-S.
               
-              PERFORM WITH TEST AFTER VARYING INT-I FROM 1 BY 1 UNTIL INT-I = INT-N
+              PERFORM WITH TEST AFTER
+              VARYING INT-I FROM 1 BY 1 UNTIL INT-I = INT-N
                      DISPLAY TRIM(STR-S)
               END-PERFORM.
-       END PROGRAM Main.
+              
+       END PROGRAM MAIN.
